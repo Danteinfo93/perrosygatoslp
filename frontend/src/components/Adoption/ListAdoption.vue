842a5487e8e323484fef5f464c25c15486eb9638
Dante <dante.ingo93@gmail.com>
2019-12-09 04:35:35 -0300

    Inicio componente Listar adopciones

---
+++ frontend/src/components/Adoption/ListAdoption.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col v-for="item of collect" :key="item.id" md="4"> 
+          <b-card
+          title="Card Title"
+          img-src="https://st3.depositphotos.com/7852538/19409/i/1600/depositphotos_194096488-stock-photo-sweet-puppy-dogo-argentino-standing.jpg"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListAdoption',
+
+  data() {
+    return {
+      collect: [1,2,3,4,5,6,7,8,9,10,11,12]
+    }
+  },
+}
+</script>
+
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    adoptions: []
   },
   mutations: {
   },
   actions: {
+
+    getAdoptions(){
+      const path = 'localhost:80000/api/v1.0/adoptions' 
+      Axios.get(path).then((response) => {
+        this.adoptions = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      }) 
+    }
   },
   modules: {
   }
